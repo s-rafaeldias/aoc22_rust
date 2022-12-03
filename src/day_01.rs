280bcaa65a9b46fe958c5ac5cd8b2f3a0ae32525
Rafael Dias <s.rafaeldias@protonmail.com>
2022-12-02 22:53:34 -0300

    feat(day_01): finish both parts

---
+++ src/day_01.rs
@@ -0,0 +1,74 @@
+use std::{collections::HashMap, usize};
+
+pub fn part_one(input: &str, bound: usize) -> usize {
+    let mut elves: HashMap<usize, usize> = HashMap::new();
+    let mut current_elve: usize = 0;
+
+    let input_parsed = input.split("\n").map(|x| x.parse::<usize>().unwrap_or(0));
+
+    for item in input_parsed {
+        elves
+            .entry(current_elve)
+            .and_modify(|v| *v += item)
+            .or_insert(item);
+
+        if item == 0 {
+            current_elve += 1;
+        }
+    }
+
+    let mut calories: Vec<usize> = elves.into_values().collect();
+
+    calories.sort();
+    calories.reverse();
+
+    let x = calories[..bound]
+        .to_vec()
+        .into_iter()
+        .reduce(|a, b| a + b)
+        .unwrap();
+    x
+}
+
+mod test {
+    use super::*;
+    use std::fs;
+
+    const INPUT: &str = r"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+    #[test]
+    fn part_one_sample() {
+        assert_eq!(24000, part_one(INPUT, 1));
+    }
+
+    #[test]
+    fn part_one_result() {
+        let input = fs::read_to_string("data/day_one.txt").unwrap();
+        assert_eq!(67622, part_one(&input, 1));
+    }
+
+    #[test]
+    fn part_two_sample() {
+        assert_eq!(45000, part_one(INPUT, 3));
+    }
+
+    #[test]
+    fn part_two_result() {
+        let input = fs::read_to_string("data/day_one.txt").unwrap();
+        assert_eq!(201491, part_one(&input, 3));
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+pub mod day_01;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("src/day_one.txt").unwrap();
+
+    let r1 = day_01::part_one(&input, 1);
+    println!("part one: {:?}", r1)
 }
